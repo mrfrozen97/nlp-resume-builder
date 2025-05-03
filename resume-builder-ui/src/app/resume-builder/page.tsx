a17c810f8466fafedfc96cfec1800ab820ea60d6
mrfrozen97 <mrfrozenpeak@gmail.com>
2025-05-03 16:34:47 -0500

    Syncing the builder and parser resumes

--- resume-builder-ui/src/app/resume-builder/page.tsx
+++ resume-builder-ui/src/app/resume-builder/page.tsx
@@ -8,11 +8,11 @@ export default function Create() {
   return (
     <Provider store={store}>
       <main className="relative h-full w-full overflow-hidden bg-gray-50">
-        <div className="grid grid-cols-3 md:grid-cols-6">
-          <div className="col-span-3">
+        <div className="grid grid-cols-3 md:grid-cols-6" >
+          {/* <div className="col-span-3">
             <ResumeForm />
-          </div>
-          <div className="col-span-3">
+          </div> */}
+          <div className="" >
             <Resume />
           </div>
         </div>
