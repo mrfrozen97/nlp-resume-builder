be54931d7011253989dd9549c3d60562b6412f17
Hamid Rouhani <hamid914@users.noreply.github.com>
2025-05-03 20:58:44 -0500

    Add scenarios to Rasa

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse, Response
 from pydantic import BaseModel
 from score_resumes import ResumeScore  # Assuming the code you provided is saved as ResumeScore.py
 import requests
 import config
 
 app = FastAPI()
+app.state.data = {}
 
 # Initialize ResumeScore once when the API starts
 resume_scorer = ResumeScore()
@@ -18,6 +19,7 @@ class ResumeResponse(BaseModel):
     normalized_score: float
     matched_skills: dict
     missing_skills: dict
+
 
 @app.post("/score_resume", response_model=ResumeResponse)
 def score_resume_endpoint(request: ResumeRequest):
@@ -41,6 +43,26 @@ def ping_bot():
 
 @app.post("/bot/chat")
 def chat_with_bot(sender: str, message: str):
+    response = requests.post(config.BOT_CHAT_URL, json={"sender": sender, "message": message})
+    if response.ok:
+        return JSONResponse(response.json())
+    else:
+        raise HTTPException(status_code=500, detail="Could not reach BOT server.")
+
+
+@app.get("/bot/webhooks/state")
+def bot_webhook_current_state(sender_id: str):
+    return app.state.data.get(sender_id, {})
+
+
+@app.post("/bot/webhooks/state")
+def bot_webhook_current_state_update(sender_id: str, state: dict):
+    app.state.data[sender_id] = state
+    return Response(status_code=status.HTTP_201_CREATED)
+
+
+@app.post("/bot/webhooks/job_title")
+def bot_webhook_job_title(sender: str, message: str):
     response = requests.post(config.BOT_CHAT_URL, json={"sender": sender, "message": message})
     if response.ok:
         return JSONResponse(response.json())
--- resume_bot/actions/actions.py
+++ resume_bot/actions/actions.py
@@ -7,21 +7,104 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.exceptions import FileIOException
+
+import requests
+
+import config
+
+
+def update(dispatcher: CollectingDispatcher, tracker: Tracker,
+           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    try:
+        response = requests.post(f"{config.API_STATE_URL}?sender_id={tracker.sender_id}", json=tracker.current_state())
+        response.raise_for_status()
+    except requests.exceptions.BaseHTTPError as e:
+        raise FileIOException(str(e)) from e
+
+
+class ActionScore(Action):
+
+    def name(self) -> Text:
+        return "action_score"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # score = tracker.get_slot("score")
+        # if not company:
+        #     dispatcher.utter_message(text="I don't know your company.")
+        # else:
+        #     dispatcher.utter_message(text=f"Your company is {company}")
+
+        try:
+            breakpoint()
+            response = requests.post(f"{config.API_SCORE_URL}", json={"resume_text": "string", "job_description": "string"})
+            response.raise_for_status()
+        except requests.exceptions.BaseHTTPError as e:
+            raise FileIOException(str(e)) from e
+        tracker.slots["score"] = response.json()["normalized_score"]
+        update(dispatcher, tracker, domain)
+        return []
+
+
+class ActionCompany(Action):
+
+    def name(self) -> Text:
+        return "action_company"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        company = tracker.get_slot("company")
+        if not company:
+            dispatcher.utter_message(text="I don't know your company.")
+        else:
+            dispatcher.utter_message(text=f"Your company is {company}")
+
+        update(dispatcher, tracker, domain)
+        return []
+
+
+class ActionSkill(Action):
+
+    def name(self) -> Text:
+        return "action_skill"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        skill = tracker.get_slot("skill")
+        if not skill:
+            dispatcher.utter_message(text="I don't know your skills.")
+        else:
+            dispatcher.utter_message(text=f"Your skill set is {skill}")
+
+        update(dispatcher, tracker, domain)
+        return []
+
+
+class ActionJobTitle(Action):
+
+    def name(self) -> Text:
+        return "action_job_title"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        job_title = tracker.get_slot("job_title")
+        if not job_title:
+            dispatcher.utter_message(text="I don't know your job title.")
+        else:
+            dispatcher.utter_message(text=f"Your job title is {job_title}")
+
+        update(dispatcher, tracker, domain)
+        return []
